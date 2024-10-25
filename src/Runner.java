d90ce108d9e28ddfba7e6334ff8c89fdeb5d1270
BT_1N3_19 <sample@bths.edu>
2024-10-25 10:02:27 -0400

    added something else

--- src/Runner.java
+++ src/Runner.java
@@ -1,5 +1,10 @@
 public class Runner {
     public static void main(String[] args) {
         System.out.println("Hello");
+        System.out.println("How are you?");
+        int x = 5;
+        int y  = 3;
+        int one = 1;
+        System.out.println("I am " + (x*y + one) + " years old.");
     }
 }
