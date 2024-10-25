a2fdb741171c02c260ee0e35613f88a3d8c0d53b
BT_1N3_19 <sample@bths.edu>
2024-10-25 10:30:52 -0400

    changed it a bunch

--- src/Runner.java
+++ src/Runner.java
@@ -1,11 +1,11 @@
 public class Runner {
     public static void main(String[] args) {
-        System.out.println("Hello");
-        System.out.println("How are you?");
-        int x = 2;
-        int y  = 1;
+        System.out.println("Hi");
+        System.out.println("How art thou?");
+        int x = 6;
+        int y  = 4;
         int z = 14;
-        System.out.println("I am " + (x*y + z) + " years old.");
-        System.out.println("How old are you?");
+        System.out.println("I am " + (x + z) + " years old.");
+        System.out.println("How was your day?");
     }
 }
---
+++ src/whateverIWant.java
@@ -0,0 +1,6 @@
+public class whateverIWant {
+    public static void main(String[] args) {
+        System.out.println("I don't know what to wriiite");
+        System.out.println("ok im done");
+    }
+}
