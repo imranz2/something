fc966b19f179b7327fed0ad07a179ae0a6c7cf12
BT_1N3_19 <sample@bths.edu>
2024-10-25 10:45:21 -0400

    added a few

--- src/Runner.java
+++ src/Runner.java
@@ -2,10 +2,11 @@ public class Runner {
     public static void main(String[] args) {
         System.out.println("Hi");
         System.out.println("How art thou?");
-        int x = 6;
-        int y  = 4;
-        int z = 14;
-        System.out.println("I am " + (x + z) + " years old.");
-        System.out.println("How was your day?");
+        System.out.println("welcome");
+        int x = 2;
+        int y  = 2;
+        int z = 1;
+        System.out.println("I am in the " + (x + z) + "th grade");
+        System.out.println("What's the weather like?");
     }
 }
